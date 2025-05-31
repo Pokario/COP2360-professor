e4c4425aef672545c7a0f1a8bdb8e08a035a344a
Pokario <jorgepereira3077@outlook.es>
2025-05-31 00:30:42 -0400

    Add files via upload
    Class Discussion

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Cat myCat = new Cat();
+        myCat.Name = "Michu";
+        myCat.Age = 5;
+        myCat.Breed = "American Shorthair";
+        
+
+        Console.WriteLine("Cat information:");
+        Console.WriteLine("Name: " + myCat.Name);
+        Console.WriteLine("Age: " + myCat.Age + " years old");
+        Console.WriteLine("Breed: " + myCat.Breed);
+
+    }
+}
+
+public class Cat
+{
+    public string Name;
+    public int Age;
+    public string Breed;
+    
+}
